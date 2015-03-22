e3e94612f74c26cb43e4bf578049adb130830e84
martini <miturbide@nextstep.com.ec>
2015-03-21 21:37:30 -0500

    1997-10

---
+++ editor1.h
@@ -0,0 +1,33 @@
+//---------------------------------------------------------------------------------------------
+// File TOOLBAR.H
+//---------------------------------------------------------------------------------------------
+#define MAINMENU	1
+#define STATUSBAR   	20
+#define TOOLBAR		30
+#define MAINMLE		500
+
+#define IDM_LEEG	99
+#define IDM_FILEMENU	100
+#define IDM_NEW		101
+#define IDM_EXIT	102
+
+#define IDM_EDITMENU	200
+#define IDM_UNDO	201
+#define IDM_CUT		203
+#define IDM_COPY	204
+#define IDM_PASTE	205
+#define IDM_CLEAR	206
+#define IDM_SEARCH	207
+#define IDM_REPLACE	208
+
+#define IDM_ABOUTMENU	300
+#define IDM_ABOUT	305
+#define IDD_ABOUT	3000
+
+#define IDM_VIEW	306
+#define IDM_DOCKMENU	307
+#define IDM_VIEWMENU	308
+#define IDM_DOCKSTATUS	309
+#define IDM_VIEWSTATUS	310
+
+#define ITEMS		2
